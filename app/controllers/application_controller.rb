8b9742571a8e5a4a91843a176d0fd6519bd09fcf
Yaroslav Mykhailiuk <yara99969@gmail.com>
2020-11-03 11:28:05 +0200

    Add additional params to sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_additional_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_additional_parameters
+    additional_params = [:role, :first_name, :last_name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: additional_params)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  enum role: %i[student teacher]
+  enum role: { student: 0, teacher: 1 }
   # Include default devise modules. Others available are:
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
