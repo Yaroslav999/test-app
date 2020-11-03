6a1d42404e76234bfb21e8703354481c6b7de7d9
Yaroslav Mykhailiuk <yara99969@gmail.com>
2020-11-03 12:21:42 +0200

    Add tests for model user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class }
+
+  it "is not valid with not valid attributes" do
+    expect(subject.new).to_not be_valid
+  end
+
+  it "is not valid with empty first name" do
+    expect(subject.new(first_name: nil, password: 'test1234')).to_not be_valid
+  end
+
+  it "is not valid with empty last name" do
+    expect(subject.new(last_name: nil, password: 'test1234')).to_not be_valid
+  end
+
+  it "is not valid with empty email" do
+    expect(subject.new(email: nil, password: 'test1234')).to_not be_valid
+  end
+
+  it "is not valid with empty email" do
+    expect(subject.new(email: 'email@email.email',
+                       first_name: 'first_name',
+                       last_name: 'last_name',
+                       password: 'test1234')).to be_valid
+  end
 end
