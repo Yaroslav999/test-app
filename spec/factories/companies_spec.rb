ee4d367ec960b011c614a2a3d02b51b0876499ab
Yaroslav Mykhailiuk <yara99969@gmail.com>
2025-02-24 18:54:08 +0200

    Added capybara tests

--- spec/factories/companies_spec.rb
+++ spec/factories/companies_spec.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :company do
     association :user
     name { Faker::Company.name }
-    country { Faker::Address.country }
+    country { 'AF' }
     initial_cash_cents { rand(1000..100_000) }
     initial_cash_currency { "USD" }
     logo { nil }
