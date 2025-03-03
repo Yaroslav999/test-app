f8d38242e40921aa52055636d30bc0fb938e9f9a
Yaroslav Mykhailiuk <yara99969@gmail.com>
2025-03-03 15:08:18 +0200

    Added caching data for cash managments

--- app/models/cash_management.rb
+++ app/models/cash_management.rb
@@ -4,7 +4,7 @@ class CashManagement < ApplicationRecord
   monetize :cash_in_cents, numericality: { greater_than: 0 }
   monetize :cash_out_cents, numericality: { greater_than: 0 }
 
-  belongs_to :company
+  belongs_to :company, touch: true
 
   validates :cash_in_cents, :cash_out_cents, :year, :month, :company, presence: true
   validates_uniqueness_of :company_id, scope: %i[month year]
